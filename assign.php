923532c2c4d8a1ff9010ad48f8e12b5aadcf4dd1
Juremy1234 <Juremyseochand@gmail.com>
2016-08-17 17:20:22 -0400

    Adding to github

---
+++ assign.php
@@ -0,0 +1,9 @@
+<?php
+
+/*echo "# assignment2" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Juremy1234/assignment2.git
+git push -u origin master
+*/
